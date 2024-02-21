02eb2b2a787f4c04eca537760949d1764b5e7339
haithamhamad@gmail.com <haithamhamad@gmail.com>
2024-02-21 12:30:28 +0000

    commit

--- Homework4/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
+++ Homework4/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
@@ -4,18 +4,18 @@
     )
 }}
 
-with tripdata as 
-( 
-    select
-        *
-    from {{ source('staging', 'fhv_tripdata') }} 
-    where EXTRACT(YEAR FROM pickup_datetime) = 2019 
-)
-select
-        cast(pickup_datetime as timestamp) as pickup_datetime,
-        cast(dropOff_datetime as timestamp) as dropoff_datetime
-from tripdata
-
+select 
+    {{ dbt.safe_cast("dispatching_base_num", api.Column.translate_type("integer")) }} as dispatching_base_num,
+	cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropOff_datetime as timestamp) as dropoff_datetime,
+    {{ dbt.safe_cast("PUlocationID", api.Column.translate_type("integer")) }} as pickup_locationid,
+    {{ dbt.safe_cast("DOlocationID", api.Column.translate_type("integer")) }} as dropoff_locationid,
+    {{ dbt.safe_cast("SR_Flag", api.Column.translate_type("integer")) }} as sr_flag,
+    {{ dbt.safe_cast("Affiliated_base_number", api.Column.translate_type("integer")) }} as affiliated_base_number
+from
+	{{ source('staging', 'fhv_tripdata') }} 
+where	
+	EXTRACT(YEAR FROM pickup_datetime) = 2019
 
 
 
